68ec806ce4b9264e6ca3cca82991f73d916f5878
udaycodess <cyberpoint805@gmail.com>
2025-08-12 12:22:57 +0530

    First commit - added HTML, CSS, and JS files

---
+++ script.js
@@ -0,0 +1,11 @@
+let userNum = prompt("Guess the game no.");
+let gameNum = 25;
+
+while(gameNum != userNum){
+  userNum = prompt("you entered wrong no. Guess the game no.");
+}
+
+console.log("you won this game!");
+
+
+
